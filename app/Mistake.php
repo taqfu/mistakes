492715fff8c370ceb06bae9373423ab2a3429176
taqfu <2buzezsn@gmail.com>
2016-11-17 10:29:40 -0800

    Accidentally started working on it a different time period

--- app/Mistake.php
+++ app/Mistake.php
@@ -9,7 +9,8 @@ class Mistake extends Model
     public function incidents (){
         return $this->hasMany('App\Incident' );
     }
-    public static function total_due(){        
+    public static function total_due(){
+        $total = 0;
         $last_sunday = date("Y-m-d H:i:s", strtotime('last Sunday'));
         $mistakes = Mistake::where('updated_at', '>', $last_sunday)->get();
         var_dump(count($mistakes));
@@ -17,7 +18,7 @@ class Mistake extends Model
             $incidents = Incident::where('mistake_id', $mistake->id)
               ->where('created_at', '>', $last_sunday)->get();
             foreach($incidents as $incident){
-                
+
             }
         }
     }
